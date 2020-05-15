7bc6fc1f425949f7e66380c8370189660bf203c2
Natalia Luiza <natalialuizas@live.com>
2020-05-15 08:59:34 -0300

    finalizado semana de jquery

--- Aula 2 - Eventos e Efeitos/Exercicios/3_val/script.js
+++ Aula 2 - Eventos e Efeitos/Exercicios/3_val/script.js
@@ -1,7 +1,14 @@
 $(document).ready(() => {
   
   $('#botao-replicar').click(() =>{
-    const name = $('#campo-nome').val()
+    const name = $('#campo-nome').val().trim()
+
+    if(name){
       $('#texto-replicado')
-            .text(name);
-  })})
+      .text('A desenvolvedora mais inscrivel desse ano será a '+ name);
+    } else {
+      alert('Informe o nome')
+    }
+      
+  })
+})
--- Aula 2 - Eventos e Efeitos/Exercicios/4_nav/script.js
+++ Aula 2 - Eventos e Efeitos/Exercicios/4_nav/script.js
@@ -1,16 +1,19 @@
 //1. Faça com que os elementos dentro do menu aparecem quando o mouse passar por eles.
 //2. Os elementos devem desaparecer quando o mouse não estiver passando por eles
 
-$(document).ready(() => {
-  $(this).mouseenter(() =>{
-    let ligado = true;
 
-    if(ligado) {
-      $('.menu2').css('display', 'block');
-      ligado = false;
-    } else {
-      $('.menu2').css('display', 'none');
-    }
-     
+$(document).ready(function() {
+    
+  $("li").hover(function() {   
+      $(this).find(".menu2").slideToggle();
   })
+  
+  
+  $("li").hover(function() {   
+      $(this).find(".menu2").slideDown(2000);
+  },
+  function() {
+      $(this).find(".menu2").slideUp("fast");
+  }
+  )
 })
